5b865dfdafd03bb11ed90d4873d95ba30de0964d
Linus Teo <fuzznuzz@gmail.com>
2016-03-12 22:47:36 +1100

    Initial Commit

--- Tutorial1/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Nuzz on 07/03/2016.
- */
-public class Main {
-    public static void main(String args[]){
-        AnagramDetector ad = new AnagramDetector();
-        ad.checkAnagram("abc", "cba");
-    }
-}
